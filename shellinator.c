9bf4e0b60014f3b3ab0ad26c596795d6dd03aea6
balemansteve <balemansteve@gmail.com>
2024-04-25 14:19:14 +0000

    shellinator.c fix

--- shellinator.c
+++ shellinator.c
@@ -13,7 +13,15 @@ void loop(void)
 		printf("<Shellinator$> ");
                 fflush(stdout);
 		line = read_line();
-		 line[strcspn(line, "\n")] = '\0';
+		/*line[strcspn(line, "\n")] = '\0';*/
+
+		int len = strlen(line);
+
+		while (len > 0 && (line[len - 1] == '\n' || line[len - 1] == ' ' || line[len - 1] == '\t')) {
+			line[len - 1] = '\0';
+			len--;
+		}
+
 		args = split_line(line);
 		arnold = execute_command(args);
 		free(line);
